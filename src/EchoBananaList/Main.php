3cd44cec5f797adcbc7b7d2f81e1d6e1bde2d651
XEmAX32 <XEmAX32@gmail.com>
2015-08-31 09:38:08 +0200

    Update Main.php

--- src/EchoBananaList/Main.php
+++ src/EchoBananaList/Main.php
@@ -8,4 +8,15 @@ use pocketmine\event\player\PlayerJoinEvent;
 
 class Main extends PluginBase{
 
-public function onJoin(PlayerJoinEvent $e){
+public function onPreLogin(PlayerPreLoginEvent $e){
+$player = $e->getPlayer();
+$name = $e->getPlayer()->getName();
+if($player->isBanned){
+foreach($this->getServer()->getOnlinePlayers as $ps){
+if($ps->hasPermission("ebl.ban"){
+$ps->sendMessage(TextFormat::BLUE . "[EBL]" $name." Ha tentato di entrare nel server anche se bannato");
+    }
+   }
+  }
+ }
+}
