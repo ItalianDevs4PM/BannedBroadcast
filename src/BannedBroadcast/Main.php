a512fc2fd68eb529983d6b9ae1b8e07aded02881
XEmAX32 <XEmAX32@gmail.com>
2015-08-31 11:56:35 +0200

    Update Main.php

--- src/BannedBroadcast/Main.php
+++ src/BannedBroadcast/Main.php
@@ -32,6 +32,7 @@ class Main extends PluginBase{
       foreach($this->getServer()->getOnlinePlayers as $ps){
         if($ps->hasPermission("bb.ban"){
           $ps->sendMessage(TextFormat::BLUE . "[BB] ".$bmessage);
+          $this->getLogger()->info(TextFormat::BLUE . "[BB] ".$bmessage);
         }
       }
     }
@@ -39,6 +40,7 @@ class Main extends PluginBase{
       foreach($this->getServer()->getOnlinePlayers as $ps){
         if($ps->hasPermission("bb.whitelist"){
           $ps->sendMessage(TextFormat::BLUE . "[BB] ".$wmessage);
+          $this->getLogger()->info(TextFormat::BLUE . "[BB] ".$wmessage);
         }
       }
     }
