b6dd2b06101275b05b502215973f3d789393a694
XEmAX32 <XEmAX32@gmail.com>
2015-08-31 09:11:21 +0200

    Create Main.php

---
+++ src/EchoBananaList/Main.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace EchoBananaList\Main;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase{
+
+public function onJoin(PlayerJoinEvent $e){
